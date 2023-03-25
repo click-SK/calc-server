c0636beccace8db362ca17b30e552478dd087446
Kichuk Rostyslav <ros.kichuk@gmail.com>
2023-03-25 13:54:35 +0200

    add dotenv

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 import * as MainController from './controllers/MainController.js';
 
 dotenv.config();
